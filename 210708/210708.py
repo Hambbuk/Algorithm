9021ecd4668a9e46208d9b77bbc24b49454746aa
Hanbin Bae <HANBIN1931@GMAIL.COM>
2021-07-08 20:56:15 +0900

    add: 프로그래머스 level2 프린터

---
+++ 210708/210708.py
@@ -0,0 +1,22 @@
+# HCI랩 알고리즘 스터디 2021.07.08
+# 프로그래머스 스택/큐 level2 프린터
+
+def solution(p, l):
+    ans = 0
+    m = max(p)
+    while True:
+        v = p.pop(0)
+        if m == v:
+            ans += 1
+            if l == 0:
+                break
+            else:
+                l -= 1
+            m = max(p)
+        else:
+            p.append(v)
+            if l == 0:
+                l = len(p)-1
+            else:
+                l -= 1
+    return ans
